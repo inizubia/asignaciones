73c5e22c0dc73e82486e2730764feeaedcd4490a
inizubia <inigozubia@gmail.com>
2017-06-05 11:26:40 +0200

    12.Kursua Editar Registros (password hutsa kudeatu)

--- src/IZJ/UserBundle/Controller/UserController.php
+++ src/IZJ/UserBundle/Controller/UserController.php
@@ -5,6 +5,8 @@ namespace IZJ\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\FormError;
 use IZJ\UserBundle\Entity\User;
 use IZJ\UserBundle\Form\UserType;
 
@@ -27,7 +29,7 @@ class UserController extends Controller
         return new Response($res);
         */
 
-        $dql = "SELECT u FROM IZJUserBundle:User u";
+        $dql = "SELECT u FROM IZJUserBundle:User u ORDER BY u.id DESC";
         $users = $em->createQuery($dql);
 
         $paginator = $this->get('knp_paginator');
@@ -70,22 +72,119 @@ class UserController extends Controller
         {
         	$password = $form->get('password')->getData();
 
-        	$encoder = $this->container->get('security.password_encoder');
-        	$encoded = $encoder->encodePassword($user, $password);
+            $passwordConstraint = new Assert\NotBlank();
+            $errorList = $this->get('validator')->validate($password, $passwordConstraint);
 
-        	$user->setPassword($encoded);
+            if(count($errorList) == 0)
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
 
-        	$em = $this->getDoctrine()->getManager();
-        	$em->persist($user);
-        	$em->flush();
+                $user->setPassword($encoded);
 
-        	$successMessage = $this->get('translator')->trans('The user has been created.');
-        	$this->addFlash('mensaje',$successMessage);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-        	return $this->redirectToRoute('izj_user_index');
+                $successMessage = $this->get('translator')->trans('The user has been created.');
+                $this->addFlash('mensaje',$successMessage);
+
+                return $this->redirectToRoute('izj_user_index');
+            }
+            else
+            {
+                $errorMessage = new FormError($errorList[0]->getMessage());
+                $form->get('password')->addError($errorMessage);
+            }
+
     	}
 
     	return $this->render('IZJUserBundle:User:add.html.twig', array('form'=> $form->createView()));
+    }
+
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('IZJUserBundle:User')->find($id);
+
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw  $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+
+        return $this->render('IZJUserBundle:User:edit.html.twig', array('user'=> $user, 'form'=> $form->createView()));
+    }
+
+    private function createEditForm(User $item)
+    {
+            $form = $this->createForm(UserType::class, $item, array(
+                'action'=> $this->generateUrl('izj_user_update', array('id' => $item->getId())),
+                'method' => 'PUT'));
+
+            return $form;
+    }
+
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('IZJUserBundle:User')->find($id);
+
+         if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw  $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $password = $form->get('password')->getData();
+            if(!empty($password))
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
+            }
+            else
+            {
+                $recoverPass = $this->recoverPass($id);
+                //print_r($recoverPass);
+                //exit();
+                $user->setPassword($recoverPass[0]['password']);                
+            }
+
+            if($form->get('role')->getData() == 'ROLE_ADMIN')
+            {
+                $user->setIsActive(1);
+            }
+
+            $em->flush();
+
+            $successMessage = $this->get('translator')->trans('The user has been modified.');
+            $this->addFlash('mensaje', $successMessage);
+            return $this->redirectToRoute('izj_user_edit', array('id'=> $user->getId()));
+        }
+        return $this->render('IZJUserBundle:User:edit.html.twig', array('user' => $user, 'form'=> $form->createView()));
+    }
+
+    private function recoverPass($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT u.password
+            FROM IZJUserBundle:User u
+            WHERE u.id = :id'    
+        )->setParameter('id', $id);
+        
+        $currentPass = $query->getResult();
+        
+        return $currentPass;
     }
 
     public function viewAction($id)
