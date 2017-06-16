117c1439b1916916e3c7345afc9607fb4cedc881
inizubia <inigozubia@gmail.com>
2017-06-16 11:50:52 +0200

    18.en KURSUA : Tareak editatu eta borratu

--- src/IZJ/UserBundle/Controller/TaskController.php
+++ src/IZJ/UserBundle/Controller/TaskController.php
@@ -65,4 +65,106 @@ class TaskController extends Controller
 
 		return $this-render('IZJUserBundle:Task:add.html.twig', array('form' => $form->createView()));
 	}
+
+	public function viewAction($id)
+	{
+		$task = $this->getDoctrine()->getRepository('IZJUserBundle:Task')->find($id);
+
+		if(!$task)
+		{
+			throw $this->createNotFoundException('The task does not exist.');
+		}
+
+		$deleteForm = $this->createCustomForm($task->getId(), 'DELETE', 'izj_task_delete');
+
+		$user = $task->getUser();
+
+		return $this->render('IZJUserBundle:Task:view.html.twig', array('task' => $task, 'user' => $user, 'delete_form' => $deleteForm->createView()));
+	}
+
+	public function editAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$task = $em->getRepository('IZJUserBundle:Task')->find($id);
+
+		if(!$task)
+		{
+			throw $this->createNotFoundException('Task not found.');
+		}
+
+		$form = $this->createEditForm($task);
+
+		return $this->render('IZJUserBundle:Task:edit.html.twig', array('task' => $task, 'form' => $form->createView()));
+	}
+
+	private function createEditForm(Task $entity)
+	{
+		$form = $this->createForm(TaskType::class, $entity, array(
+			'action' => $this->generateUrl('izj_task_update', array('id' => $entity->getId())),
+			'method' => 'PUT'
+		));
+
+		return $form;
+	}
+
+	public function updateAction($id, Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$task = $em->getRepository('IZJUserBundle:Task')->find($id);
+
+		if(!$task)
+		{
+			throw $this->createNotFoundException('Task not found.');
+		}
+
+		$form = $this->createEditForm($task);
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() and $form->isValid())
+		{
+			$task->setStatus(0);
+			$em->flush();
+			$successMessage = $this->get('translator')->trans('The task has been modified.');
+			$this->addFlash('mensaje', $successMessage);
+			return $this->redirectToRoute('izj_task_edit', array('id' => $task->getId()));
+		}
+
+		return $this->render('IZJUserBundle:Task:edit.html.twig', array('task' => $task, 'form' => $form->createView()));
+	}
+
+	public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $task = $em->getRepository('IZJUserBundle:Task')->find($id);
+        
+        if(!$task)
+        {
+            throw $this->createNotFoundException('task not found');
+        }
+        
+        $form = $this->createCustomForm($task->getId(), 'DELETE', 'izj_task_delete');
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() and $form->isValid())
+        {
+            $em->remove($task);
+            $em->flush();
+            
+            $successMessage = $this->get('translator')->trans('The task has been deleted.');
+            $this->addFlash('mensaje', $successMessage); 
+            
+            return $this->redirectToRoute('izj_task_index');
+        }
+    }
+
+	private function createCustomForm($id, $method, $route)
+	{
+		return $this->createFormBuilder()
+			->setAction($this->generateUrl($route, array('id' => $id)))
+			->setMethod($method)
+			->getForm();
+	}
 }
